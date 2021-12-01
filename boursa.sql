e381f61f831f9975edc3da5e0bff139570d6cc88
Mohamed <mbeirouck@gmail.com>
2021-12-01 19:13:23 +0000

    rien

--- boursa.sql
+++ boursa.sql
@@ -1,16 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 01 nov. 2021 à 21:10
--- Version du serveur :  5.7.26
--- Version de PHP :  7.3.5
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Mohamed beirouk
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
